60b12952c37d265b8bd51dd7423a899568bd4a4c
guilherme <guilherme.bassi.sp@gmail.com>
2023-01-13 20:29:11 +1030

    update tests

--- test/any_date_test.dart
+++ test/any_date_test.dart
@@ -1,4 +1,5 @@
 import 'package:any_date/any_date.dart';
+import 'package:any_date/src/date_range.dart';
 import 'package:test/test.dart';
 
 /// used to run tests on a wide range of dates
@@ -6,10 +7,6 @@ const exhaustiveTests = bool.fromEnvironment('exhaustive', defaultValue: true);
 
 void main() {
   group('basic AnyDate.parse tests', () {
-    setUp(() {
-      // Additional setup goes here.
-    });
-
     test('matches DateTime.parse', () {
       final d = DateTime(2023, 1, 2, 3, 4, 5, 6, 7).toString();
 
@@ -20,14 +17,12 @@ void main() {
   group(
     'exhaustive AnyDate.parse tests',
     () {
-      setUp(() {
-        // Additional setup goes here.
-      });
+      final range = DateTimeRange(start: DateTime(1999), end: DateTime(2005));
 
       test('matches DateTime.parse', () {
-        final d = DateTime(2023, 1, 2, 3, 4, 5, 6, 7).toString();
-
-        expect(DateTime.parse(d), AnyDate.parse(d));
+        for (var d in range.days) {
+          expect(DateTime.parse('$d'), AnyDate.parse('$d'));
+        }
       });
     },
     skip: !exhaustiveTests,
