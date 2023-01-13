afe13b9062c8f3c33137ffa95f3d72e077c5f4ff
guilherme <guilherme.bassi.sp@gmail.com>
2023-01-13 20:16:58 +1030

    created date range object

---
+++ lib/src/date_range.dart
@@ -0,0 +1,47 @@
+/// A range between two DateTime objects.
+/// Inspired by Flutter > Material > DateTimeRange class
+class DateTimeRange {
+  /// Start DateTime of this DateTimeRange.
+  late final DateTime start;
+
+  /// End DateTime of this DateTimeRange.
+  late final DateTime end;
+
+  /// Creates a range for the given start and end values. [end] must be later than [start]
+  DateTimeRange({
+    required DateTime start,
+    required DateTime end,
+  }) {
+    assert(!start.isAfter(end));
+    // if user passes wrong values in production code, they are corrected here
+    if (start.isAfter(end)) {
+      this.start = end;
+      this.end = start;
+    }
+    // otherwise default to normal case
+    else {
+      this.start = start;
+      this.end = end;
+    }
+  }
+
+  /// Returns a [Duration] of the time between [start] and [end].
+  ///
+  /// See [DateTime.difference] for more details.
+  Duration get duration => end.difference(start);
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other is DateTimeRange &&
+            other.runtimeType == runtimeType &&
+            other.start == start &&
+            other.end == end);
+  }
+
+  @override
+  int get hashCode => Object.hashAll([start, end]);
+
+  @override
+  String toString() => '$start - $end';
+}
