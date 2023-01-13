57bbffe47d7d9356283daedd07c88422b1dd05a5
guilherme <guilherme.bassi.sp@gmail.com>
2023-01-13 19:31:08 +1030

    init project

---
+++ example/any_date_example.dart
@@ -0,0 +1,6 @@
+import 'package:any_date/any_date.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/any_date.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library any_date;
+
+export 'src/any_date_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
