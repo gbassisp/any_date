f6bbfa8349f242c11a2d9e9eaa928f32d29d32d5
gbassisp <guilherme.bassi.sp@gmail.com>
2023-08-28 19:25:48 +0930

    fix: dart 2.12 linter warnings

--- example/any_date_example.dart
+++ example/any_date_example.dart
@@ -1,6 +1,6 @@
 import 'package:any_date/any_date.dart';
 
 void main() {
-  final date = AnyDate().parse('2023-May-23');
+  const date = AnyDate().parse('2023-May-23');
   print('awesome: $date');
 }
