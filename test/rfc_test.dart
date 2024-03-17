8f7e01cd129ca2411537530924bc88e9b1022ab3
guilherme <guilherme.bassi.sp@gmail.com>
2024-03-17 13:31:33 +1030

    test: ensure unix seconds are always correctly parsed

--- test/rfc_test.dart
+++ test/rfc_test.dart
@@ -1,16 +1,17 @@
 import 'package:any_date/src/any_date_base.dart';
-import 'package:lean_extensions/dart_essentials.dart';
+import 'package:any_date/src/date_range.dart';
 import 'package:test/test.dart';
 
 void main() {
+  const secondsLimit = 8640000000;
   final dates = [
     DateTime(1901),
     DateTime(1960),
     // 1969-09-23 09:30:00.000 (lower limit of ambiguity)
-    DateTime.fromMillisecondsSinceEpoch(-8640000001),
+    DateTime.fromMillisecondsSinceEpoch(-secondsLimit - 1),
     DateTime.fromMicrosecondsSinceEpoch(0),
     // 1970-04-11 09:30:00.000 (upper limit of ambiguity)
-    DateTime.fromMillisecondsSinceEpoch(8640000001),
+    DateTime.fromMillisecondsSinceEpoch(secondsLimit + 1),
     DateTime(1980),
     DateTime.now(),
     DateTime(2038),
@@ -91,17 +92,51 @@ void main() {
     // unix time is supposed to be in seconds, but people started using milli,
     // micro and nano seconds... this means that a small number (less than
     // 8640000000 seconds) can be any of these
-    group('ambiguous  unix time', () {
+    group('ambiguous unix time', () {
       const parser = AnyDate();
       test('seconds is always right', () {
-        for (final t in range(-8640000000, 8640000000, 12345)) {
-          final res = parser.tryParse(t.toString());
-          final expected = DateTime.fromMillisecondsSinceEpoch(t * 1000);
+        for (final d in DateTimeRange(
+          start: DateTime(1801),
+          end: DateTime(2138),
+        ).hours) {
+          final t = d.secondsSinceEpoch;
+          final s = parser.tryParse(t.toString());
+          final expected = d;
+
           expect(
-            res,
+            s,
             equals(expected),
-            reason: 'parsing $t seconds resulted in $res instead of $expected',
+            reason: 'parsing $d seconds resulted in $s instead of $expected',
           );
+        }
+      });
+
+      test('milli, micro and nanoseconds fail when too small', () {
+        for (final d in DateTimeRange(
+          // this is the limit where seconds and milliseconds start to get
+          // mixed up. we start getting milliseconds parsed as seconds
+          start: DateTime.fromMillisecondsSinceEpoch(-secondsLimit),
+          end: DateTime.fromMillisecondsSinceEpoch(secondsLimit),
+        ).hours) {
+          final ms = d.millisecondsSinceEpoch;
+          final us = d.microsecondsSinceEpoch;
+          final ns = d.nanosecondsSinceEpoch;
+          final msd = parser.tryParse(ms.toString());
+          final usd = parser.tryParse(us.toString());
+          final nsd = parser.tryParse(ns.toString());
+          final reason = 'parsing $d resulted in:\n'
+              'milli: $ms into $msd'
+              'micro: $us into $usd'
+              'milli: $ns into $nsd';
+
+          // except for 0 unix time. this is the same for every case
+          if (ms == 0) {
+            continue;
+          }
+
+          expect(msd, isNot(equals(d)), reason: reason);
+          expect(usd, isNot(equals(d)), reason: reason);
+          expect(nsd, isNot(equals(d)), reason: reason);
         }
       });
     });
@@ -111,4 +146,10 @@ void main() {
 extension _UnixTime on DateTime {
   int get secondsSinceEpoch => millisecondsSinceEpoch ~/ 1000;
   int get nanosecondsSinceEpoch => microsecondsSinceEpoch * 1000;
+}
+
+extension _IterableRange on DateTimeRange {
+  Iterable<DateTime> get hours => every(const Duration(hours: 1));
+  // Iterable<DateTime> get minutes => every(const Duration(minutes: 1));
+  // Iterable<DateTime> get seconds => every(const Duration(seconds: 1));
 }
