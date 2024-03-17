2874c162175ed3ba4a55be8c1949963ffb944d85
guilherme <guilherme.bassi.sp@gmail.com>
2024-03-17 21:37:47 +1030

    test: added tests to enforce rfc 822 and 2822

--- test/rfc_test.dart
+++ test/rfc_test.dart
@@ -231,6 +231,55 @@ void main() {
       expect(parse(input), equals(expected));
     });
   });
+
+  group('RFC 822 and RFC 2822 Date Time Parsing', () {
+    test('Valid RFC 822 date time string (GMT)', () {
+      const input = 'Sun, 17 Mar 24 12:30:45 GMT';
+      final expected = DateTime.utc(2024, 3, 17, 12, 30, 45);
+      expect(parse(input), equals(expected));
+    });
+
+    test('Valid RFC 822 date time string with timezone offset', () {
+      const input = 'Sun, 17 Mar 24 12:30:45 +0700';
+      final expected = DateTime.utc(2024, 3, 17, 5, 30, 45); // Offset adjusted
+      expect(parse(input), equals(expected));
+    });
+
+    test('Valid RFC 2822 date time string (GMT)', () {
+      const input = 'Sun, 17 Mar 2024 12:30:45 GMT';
+      final expected = DateTime.utc(2024, 3, 17, 12, 30, 45);
+      expect(parse(input), equals(expected));
+    });
+
+    test('Valid RFC 2822 date time string with timezone offset', () {
+      const input = 'Sun, 17 Mar 2024 12:30:45 +0700';
+      final expected = DateTime.utc(2024, 3, 17, 5, 30, 45); // Offset adjusted
+      expect(parse(input), equals(expected));
+    });
+    test('Valid RFC 822 date time string (GMT) with milliseconds', () {
+      const input = 'Sun, 17 Mar 24 12:30:45.123 GMT';
+      final expected = DateTime.utc(2024, 3, 17, 12, 30, 45, 123);
+      expect(parse(input), equals(expected));
+    });
+
+    test('Valid RFC 822 date time string with timezone with milliseconds', () {
+      const input = 'Sun, 17 Mar 24 12:30:45.123 +0700';
+      final expected = DateTime.utc(2024, 3, 17, 5, 30, 45, 123);
+      expect(parse(input), equals(expected));
+    });
+
+    test('Valid RFC 2822 date time string (GMT) with milliseconds', () {
+      const input = 'Sun, 17 Mar 2024 12:30:45.123 GMT';
+      final expected = DateTime.utc(2024, 3, 17, 12, 30, 45, 123);
+      expect(parse(input), equals(expected));
+    });
+
+    test('Valid RFC 2822 date time string with timezone with milliseconds', () {
+      const input = 'Sun, 17 Mar 2024 12:30:45.123 +0700';
+      final expected = DateTime.utc(2024, 3, 17, 5, 30, 45, 123);
+      expect(parse(input), equals(expected));
+    });
+  });
 }
 
 extension _UnixTime on DateTime {
