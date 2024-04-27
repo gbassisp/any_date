df9ad80e90101ae8b21a795b96153bae070a7df1
guilherme <guilherme.bassi.sp@gmail.com>
2024-04-27 16:24:46 +0930

    feat: added logic to remove and get timezone offset

--- lib/src/time_zone_logic.dart
+++ lib/src/time_zone_logic.dart
@@ -1,5 +1,23 @@
 import 'package:meta/meta.dart';
 
+/// removes timezone offset from string
+@internal
+String removeTimezoneOffset(String timestamp) {
+  final tzExp = _expressions.last;
+  final tz = tzExp.stringMatch(replaceUtc(timestamp));
+
+  return tz ?? timestamp;
+}
+
+/// gets timezone offset if existing, else returns null
+@internal
+String? getTimezoneOffset(String timestamp) {
+  final tzExp = _expressions.last;
+  final tz = tzExp.stringMatch(replaceUtc(timestamp));
+
+  return tz;
+}
+
 /// replace 'UTC' or 'GMT' to 'Z'
 @internal
 String replaceUtc(String formattedString) {
@@ -38,5 +56,6 @@ final _expressions = {
   RegExp(r'\s*utc', caseSensitive: false),
   RegExp(r'\s*gmt', caseSensitive: false),
   RegExp('Z', caseSensitive: false),
+  // RegExp(r'\s*[\+-]\d{2}\:?\d{0,2}\s*$', caseSensitive: false),
   RegExp(r'\s+[\+-]\d+', caseSensitive: false),
 };
