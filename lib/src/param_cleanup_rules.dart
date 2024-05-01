972a5785871bef61a57f340036b637f9ea0c9ab3
guilherme <guilherme.bassi.sp@gmail.com>
2024-05-02 08:23:12 +0930

    feat: added cleanup rules files

---
+++ lib/src/param_cleanup_rules.dart
@@ -0,0 +1,6 @@
+import 'package:any_date/src/any_date_rules_model.dart';
+import 'package:meta/meta.dart';
+
+/// entry point for all clean-up rules
+@internal
+final cleanupRules = MultipleRules([]);
